b4df9da2ad373956303cb09f08dfb49ad3feb3ad
Kevin Setiabudi <kevinsetiabudi20@gmail.com>
2021-08-26 17:11:50 +0700

    create function swap tile
    save each row and column to a list and make a function to swap tile for row and for column

--- 2048/Assets/Script/Game.cs
+++ 2048/Assets/Script/Game.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     private Tile[,] AllTiles = new Tile[4, 4];
+    private List<Tile[]> columns = new List<Tile[]>();
+    private List<Tile[]> rows = new List<Tile[]>();
     private List<Tile> EmptyTiles = new List<Tile>();
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,42 @@ public class Game : MonoBehaviour
             AllTiles[t.indRow, t.indCol] = t;
             EmptyTiles.Add(t);
         }
+        columns.Add(new Tile[] { AllTiles[0, 0], AllTiles[1, 0], AllTiles[2, 0], AllTiles[3, 0] });
+        columns.Add(new Tile[] { AllTiles[0, 1], AllTiles[1, 1], AllTiles[2, 1], AllTiles[3, 1] });
+        columns.Add(new Tile[] { AllTiles[0, 2], AllTiles[1, 2], AllTiles[2, 2], AllTiles[3, 2] });
+        columns.Add(new Tile[] { AllTiles[0, 3], AllTiles[1, 3], AllTiles[2, 3], AllTiles[3, 2] });
+
+        rows.Add(new Tile[] { AllTiles[0, 0], AllTiles[0, 1], AllTiles[0, 2], AllTiles[0, 3] });
+        rows.Add(new Tile[] { AllTiles[1, 0], AllTiles[1, 1], AllTiles[1, 2], AllTiles[1, 3] });
+        rows.Add(new Tile[] { AllTiles[2, 0], AllTiles[2, 1], AllTiles[2, 2], AllTiles[2, 3] });
+        rows.Add(new Tile[] { AllTiles[3, 0], AllTiles[3, 1], AllTiles[3, 2], AllTiles[3, 3] });
+    }
+
+    bool MakeOneMoveDownIndex(Tile[] LineOfTiles)
+    {
+        for (int i = 0; i < LineOfTiles.Length-1; i++)
+        {
+            if (LineOfTiles[i].Number == 0 && LineOfTiles[i+1].Number != 0)
+            {
+                LineOfTiles[i].Number = LineOfTiles[i + 1].Number;
+                LineOfTiles[i + 1].Number = 0;
+                return true;
+            }
+        }
+        return false;
+    }
+    bool MakeOneMoveUpIndex(Tile[] LineOfTiles)
+    {
+        for (int i = LineOfTiles.Length-1; i > 0; i--)
+        {
+            if (LineOfTiles[i].Number == 0 && LineOfTiles[i-1].Number != 0)
+            {
+                LineOfTiles[i].Number = LineOfTiles[i - 1].Number;
+                LineOfTiles[i - 1].Number = 0;
+                return true;
+            }
+        }
+        return false;
     }
 
     void Generate()
