a465c2f6afefeb8a9b31f82fb55e314912a7b68d
Kevin Setiabudi <kevinsetiabudi20@gmail.com>
2021-08-30 23:07:42 +0700

    tile now can move
    add some script to actually use the swaping tile

--- 2048/Assets/Script/Game.cs
+++ 2048/Assets/Script/Game.cs
@@ -21,7 +21,7 @@ public class Game : MonoBehaviour
         columns.Add(new Tile[] { AllTiles[0, 0], AllTiles[1, 0], AllTiles[2, 0], AllTiles[3, 0] });
         columns.Add(new Tile[] { AllTiles[0, 1], AllTiles[1, 1], AllTiles[2, 1], AllTiles[3, 1] });
         columns.Add(new Tile[] { AllTiles[0, 2], AllTiles[1, 2], AllTiles[2, 2], AllTiles[3, 2] });
-        columns.Add(new Tile[] { AllTiles[0, 3], AllTiles[1, 3], AllTiles[2, 3], AllTiles[3, 2] });
+        columns.Add(new Tile[] { AllTiles[0, 3], AllTiles[1, 3], AllTiles[2, 3], AllTiles[3, 3] });
 
         rows.Add(new Tile[] { AllTiles[0, 0], AllTiles[0, 1], AllTiles[0, 2], AllTiles[0, 3] });
         rows.Add(new Tile[] { AllTiles[1, 0], AllTiles[1, 1], AllTiles[1, 2], AllTiles[1, 3] });
@@ -81,6 +81,25 @@ public class Game : MonoBehaviour
 
     public void Move(MoveDirection md)
     {
-        
+        for (int i = 0; i < rows.Count; i++)
+        {
+            switch (md)
+            {
+                case MoveDirection.Left:
+                    while (MakeOneMoveDownIndex(rows[i])) { }
+                    break;
+                case MoveDirection.Right:
+                    while (MakeOneMoveUpIndex(rows[i])) { }
+                    break;
+                case MoveDirection.Up:
+                    while (MakeOneMoveDownIndex(columns[i])) { }
+                    break;
+                case MoveDirection.Down:
+                    while (MakeOneMoveUpIndex(columns[i])) { }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
