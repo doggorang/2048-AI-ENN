677c6d1516b9dc1d23f4597e67dc3401ed29bdfc
Kevin Setiabudi <kevinsetiabudi20@gmail.com>
2021-08-31 17:16:15 +0700

    score tracker
    Script to keep track the score

---
+++ 2048/Assets/Script/ScoreTracker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour
+{
+    private int score;
+    public static ScoreTracker Instance;
+    public Text ScoreText;
+    public Text HighScoreText;
+    void Awake()
+    {
+        Instance = this;
+    }
+}
