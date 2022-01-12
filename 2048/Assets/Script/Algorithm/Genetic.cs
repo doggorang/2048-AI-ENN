b41f8e8d612c0c16392501e7edf0176e8f7bf9d7
Kevin Setiabudi <kevinsetiabudi20@gmail.com>
2022-01-12 22:48:53 +0700

    Create class ind & genetic
    init class for genetic & individual

---
+++ 2048/Assets/Script/Algorithm/Genetic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genetic : MonoBehaviour
+{
+    private List<Individual> AllIndivual = new List<Individual>();
+    private int population = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random.InitState(218116692);
+        for (int i = 0; i < population; i++)
+        {
+            float[] tempW = new float[6];
+            for (int j = 0; j < 6; j++)
+            {
+                tempW[j] = Random.value;
+            }
+            AllIndivual.Add(new Individual(tempW));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2048/Assets/Script/Individual.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Individual : MonoBehaviour
+{
+    public float Fitness;
+    private float[] weights = new float[6];
+    public Individual(float[] weights)
+    {
+        this.weights = weights;
+    }
+    public float[] Weights
+    {
+        get
+        {
+            return weights;
+        }
+        set
+        {
+            weights = value;
+            Fitness = 1;
+        }
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
